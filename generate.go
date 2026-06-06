65a0282c78c38d9f282b125483a9b5a1b233c86b
zaaack <zaaack@qq.com>
2026-06-06 11:29:54 +0800

    feat: add cross-compilation for Windows in WSL

--- generate.go
+++ generate.go
@@ -1,3 +1,4 @@
 package main
 
-//go:generate go build -o ./go-bin.exe ./cmd/go-bin
+//go:generate go build -o ./go-bin ./cmd/go-bin
+//go:generate sh -c "GOOS=windows GOARCH=amd64 go build -o ./go-bin.exe ./cmd/go-bin"
